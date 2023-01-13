8a67b8a806ffd548b84d2eb1ccf2fd487e459235
Dan Murphy <ngx.morpha@gmail.com>
2023-01-13 11:53:42 +1100

    got the Config and Wifi stuff working.

--- TimeController.py
+++ TimeController.py
@@ -1,22 +1,43 @@
 import alarm
 import board
 import digitalio
-import neopixel
 import time
 
-def sleep(self, TimeToSleep):
-    np_power = digitalio.DigitalInOut(board.NEOPIXEL_POWER)
-    np_power.switch_to_output(value=False)
-    np = neopixel.NeoPixel(board.NEOPIXEL, 1)
-    np[0] = (50, 50, 50)
-    time.sleep(0.5)
-    np[0] = (0, 0, 0)
-    time.sleep(0.5)
-    np[0] = (50, 50, 50)
-    time.sleep(1)
-    np[0] = (0, 0, 0)
-    time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + TimeToSleep)
-    alarm.exit_and_deep_sleep_until_alarms(time_alarm)
+class TimeController:
+    def __init__(self, config):
+        self.sleepTime = config["sleep_time"]
+        self.useExternalWakeUp = config["use_external_wake_up"] ##TODO the plan is to have the BUTTON wake the bindicator on press.
+        self.alertBegin = config["alert_begin"]
 
-def checkDate(self):
-    print('check date')
+    def sleep(self, timeToSleep = None):
+        if timeToSleep == None:
+            timeToSleep = self.sleepTime
+
+        time.sleep(timeToSleep)
+
+    def lightsleep(self, timeToSleep = None, pin_alarm = None):
+        if timeToSleep == None:
+            timeToSleep = self.sleepTime
+
+        time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + timeToSleep)
+
+        if self.useExternalWakeUp == True and pin_alarm != None:
+            alarm.light_sleep_until_alarms(time_alarm, pin_alarm)
+        else:
+            alarm.light_sleep_until_alarms(time_alarm)
+
+    def deepsleep(self, timeToSleep = None, pin_alarm = None):
+        #Deepsleep restarts the board when it wakes... but you CAN preserve pin states whilst in sleep.
+        if timeToSleep == None:
+            timeToSleep = self.sleepTime
+
+        print("================")
+        print("going to sleep now...")
+
+        time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + timeToSleep)
+
+        if self.useExternalWakeUp == True and pin_alarm != None:
+            alarm.exit_and_deep_sleep_until_alarms(time_alarm, pin_alarm)
+        else:
+            alarm.exit_and_deep_sleep_until_alarms(time_alarm)
+
---
+++ config.py
@@ -0,0 +1,14 @@
+config = {
+    'timezone_offset': 10,
+    'glowbit' : {
+        'brightness': 0.5
+        },
+    'button' : {
+        'debounce': 100
+        },
+    'time' : {
+        'sleep_time': 5,
+        'alert_begin': "12:00",
+        'use_external_wake_up': True
+        }
+    }
